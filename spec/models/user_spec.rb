9df2ed3b55c0614f5bdc2f8c8406985a23097953
Emika Ijuin <emikaijuin@gmail.com>
2018-10-03 17:22:25 +0800

    Add tests for User#initialize

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
-require 'test_helper'
+require 'rails_helper'
 
-class UserTest < ActiveSupport::TestCase
-    Given (:user) {User.new}
+RSpec.describe User, type: :model do
+
+  Given(:first_name) { "John" }
+  Given(:last_name) { "Doe" }
+  Given(:email) { "example@email.com" }
+  Given(:password) { "password" }
+  Given(:user_information) { {first_name: first_name, last_name: last_name, email: email} }
+  Given(:user) { User.new(user_information) }
+
+  describe "#initialize" do 
+      context "It creates a user with the given information" do
+          Given { user.password = password } 
+          When { user.save } 
+          Then { expect(User.where(user_information)).to exist }
+      end
+
+      context "It requires first_name, last_name, email, and password to save" do
+        Given (:incorrect_user) { User.new }
+        Then { expect(user.save).to be(false) }
+      end
+  end
+
 end
