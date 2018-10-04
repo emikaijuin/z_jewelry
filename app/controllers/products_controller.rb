5ba7ffa464dcc2da5421083f7b59f5183861a423
Emika Ijuin <emikaijuin@gmail.com>
2018-10-04 11:24:33 +0800

    Set action filter to set product before :show, :edit, :update, and :destroy. Set @products and @product for #index and #new.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,12 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+  
   def index
+    @products ||= Product.all
   end
 
   def new
+    @product = Product.new
   end
 
   def create
