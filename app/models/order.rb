f8b08ff199491b7d592b3ff244a3ab7af747756a
Emika Ijuin <emikaijuin@gmail.com>
2018-10-17 11:33:27 +0800

    Enum status as unconfirmed: 0, pending: 1, confirmed: 2

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,10 @@
 class Order < ApplicationRecord
+  validate :require_address_before_confirming
+  enum status: [:unconfirmed, :pending, :confirmed]
+
+  def require_address_before_confirming
+    if self.confirmed? && !self.address_id
+      errors.add("You must select an address before confirming order.")
+    end
+  end
 end
