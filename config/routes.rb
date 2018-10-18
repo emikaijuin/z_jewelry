f9e95506eb825bee81a971e64ad8c8259004ad6e
Emika Ijuin <emikaijuin@gmail.com>
2018-10-18 10:40:41 +0800

    Add orders routes nested under users; create order index under product show for admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   root :to => "products#index"
-  devise_for :users
-  resources :products
+  devise_for :users 
+  
+  resources :users do
+    resources :orders
+  end
+
+  resources :products do
+    resources :orders, only: [:index] # Admin can view all orders associated to a product
+  end
 end
