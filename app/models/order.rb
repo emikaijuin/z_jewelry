61fba6532fa2ff42f13e5cbb3ec4c75016999605
Emika Ijuin <emikaijuin@gmail.com>
2018-10-18 10:38:39 +0800

    Validation: - Validate that address has been set before confirming order
    Confirmation Status Enum:
    - Enumerate status column as unconfirmed, pending, and confirmed

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,15 @@
 class Order < ApplicationRecord
-  validate :require_address_before_confirming
-  enum status: [:unconfirmed, :pending, :confirmed]
 
-  def require_address_before_confirming
+  ## Validations 
+  validate :require_address_before_confirming
+  
+  def require_address_before_confirming # Cannot confirm an order unless address has been set
     if self.confirmed? && !self.address_id
       errors.add("You must select an address before confirming order.")
     end
   end
+
+  ## Confirmation Status
+  enum status: [:unconfirmed, :pending, :confirmed]
 end
+
