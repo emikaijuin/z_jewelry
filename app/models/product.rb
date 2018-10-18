4a6cd73db78e901951aff2f5b928f895c8b8785e
Emika Ijuin <emikaijuin@gmail.com>
2018-10-18 11:54:13 +0800

    Create associations between products, orders, and order items

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
+  ## Associations
+  has_many :order_items
+  has_many :orders, through: :order_items
+  ## Validations
   validates :description, presence: true
 end
