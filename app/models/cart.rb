e53ea0cab51fa6bf15bd3b48ef2f3b5ab4c62fce
Emika Ijuin <emikaijuin@gmail.com>
2018-10-18 14:51:54 +0800

    Create optional association to user

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,5 @@
 class Cart < ApplicationRecord
   has_many :cart_items
   has_many :products, through: :cart_items
+  belongs_to :user, optional: true # Carts can be associated with users if users are logged in, but will be nil if shopping as a guest
 end
