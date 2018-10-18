c047b6ec500e788e29efa5c507d5679656639bbe
Emika Ijuin <emikaijuin@gmail.com>
2018-10-18 12:48:01 +0800

    Set many-to-many associations between carts, products, through cart_items

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,13 @@
 class Product < ApplicationRecord
   ## Associations
+  # Products belong to many orders
   has_many :order_items
   has_many :orders, through: :order_items
+
+  # Products belong to many carts
+  has_many :cart_items
+  has_many :carts, through: :cart_items
+  
   ## Validations
   validates :description, presence: true
 end
